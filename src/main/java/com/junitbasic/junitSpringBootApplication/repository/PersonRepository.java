fba21086095bc1e1bd5bdcf1dc03cee55165afcf
Harmeet Singh Gorowada <harmeetsingh7276@gmail.com>
2023-05-24 21:17:31 +0530

    updated query

--- src/main/java/com/junitbasic/junitSpringBootApplication/repository/PersonRepository.java
+++ src/main/java/com/junitbasic/junitSpringBootApplication/repository/PersonRepository.java
@@ -3,9 +3,11 @@ package com.junitbasic.junitSpringBootApplication.repository;
 import com.junitbasic.junitSpringBootApplication.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface PersonRepository extends JpaRepository<Person,Integer> {
-//    @Query("SELECT CASE WHEN COUNT(s) > 0 THEN TRUE ELSE FALSE END FROM PERSON s WHERE s.personId = ?1")
-//    boolean isPersonExistsById(Integer id);
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+    @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM Person p WHERE p.id = :id")
+    boolean isPersonExistsById(@Param("id") Integer id);
+
 
 }
